afbdb735ca08dd99861a1a5ec24de45a6c1d2b2b
Kubar Ekaterina <balakinakata002@gmail.com>
2023-04-18 20:48:31 +0400

    Выполнено, только 2 задания

---
+++ Работа2/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+//Console.WriteLine("Введите трёхзначное число: ");
+//int number=Convert.ToInt32(Console.ReadLine());
+//int resilt=number/10;
+//int resilt2=resilt%10;
+//Console.WriteLine(resilt2);
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+//Console.WriteLine("Введите число");
+//int numder=Convert.ToInt32(Console.ReadLine());
+//if(numder==1)
+//{
+    //Console.WriteLine("Понедельник,НЕТ");
+//}
+//else if(numder==2)
+//{
+    //Console.WriteLine("Вторник,НЕТ");
+//}
+//else if(numder==3)
+//{
+   // Console.WriteLine("Среда,НЕТ");
+//}
+//else if(numder==4)
+//{
+   // Console.WriteLine("Четверг,НЕТ");
+//}
+//else if(numder==5)
+//{
+   // Console.WriteLine("Пятница,НЕТ");
+//}
+//else if(numder==6)
+//{
+   // Console.WriteLine("Суббота,ДА");
+//}
+//else if(numder==7)
+//{
+  //  Console.WriteLine("Воскресенье,ДА");
+//}
+//else
+//{
+  //  Console.WriteLine("Число не относится к дням неделям");
+//}
+
+
+
